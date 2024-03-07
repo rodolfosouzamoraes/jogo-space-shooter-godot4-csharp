0d6fd8af21f6501811610d8ee596fda210b47ad2
rodolfosouzamoraes <rodolfomsouza@gmail.com>
2024-03-07 15:35:13 -0300

    Movimentação do Laser

---
+++ Scripts/Laser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Godot;
+
+
+public partial class Laser : Node2D
+{
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        Vector2 velocity = new Vector2(0, -1 * 500 * (float)delta).Rotated(Rotation);
+        Position += velocity;
+    }
+
+}
