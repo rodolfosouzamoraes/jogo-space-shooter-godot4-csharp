8c156b06848a53f0298cd10ad7607a0bf023825f
rodolfosouzamoraes <rodolfomsouza@gmail.com>
2024-03-08 15:48:29 -0300

    Aula 3

--- Scripts/Laser.cs
+++ Scripts/Laser.cs
@@ -4,9 +4,19 @@ using Godot;
 
 public partial class Laser : Node2D
 {
+    Timer timer;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        Callable callable = Callable.From(() => DestroyLaser());
+
+        timer = new Timer();
+        timer.OneShot = true;
+        timer.WaitTime = 1;
+        timer.Autostart = true;
+        timer.Connect("timeout", callable);
+        AddChild(timer);
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,6 +24,11 @@ public partial class Laser : Node2D
     {
         Vector2 velocity = new Vector2(0, -1 * 500 * (float)delta).Rotated(Rotation);
         Position += velocity;
+    }
+
+    private void DestroyLaser()
+    {
+        QueueFree();
     }
 
 }
--- Scripts/Nave.cs
+++ Scripts/Nave.cs
@@ -3,22 +3,59 @@ using System;
 
 public partial class Nave : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	[Export] PackedScene laserNode = ResourceLoader.Load<PackedScene>("res://Prefabs/laser.tscn");
+
+	Sprite2D body;
+    Timer timer;
+    bool isFire;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
 	{
-	}
+
+        body = GetNodeOrNull<Sprite2D>("Body");
+
+		isFire = false;
+
+		Callable callable = Callable.From(() => EnableFire());
+
+		timer = new Timer();
+		timer.OneShot = true;
+		timer.WaitTime = 0.1;
+		timer.Autostart = true;
+		timer.Connect("timeout", callable);
+		AddChild(timer);
+    }
+
+	private void EnableFire()
+	{
+		isFire = true;
+    }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-	}
+		if(isFire == true)
+		{
+            Fire();
+			isFire = false;
+			timer.Start();
+        }
+		
+    }
 
     public override void _Input(InputEvent @event)
     {
         if(@event is InputEventMouseMotion motionEvent)
 		{
-            Sprite2D body = GetNodeOrNull<Sprite2D>("Body");
             body.Position = motionEvent.Position;
         }
     }
+
+	private void Fire()
+	{
+		Node laser = laserNode.Instantiate();
+		AddChild(laser);
+		laser.GetNode<Node2D>(laser.GetPath()).Position = new Vector2(body.Position.X, body.Position.Y);
+	}
 }
