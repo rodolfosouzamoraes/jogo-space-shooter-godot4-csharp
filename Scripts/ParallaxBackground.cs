fab11cf85d42da427129b2ef764e236c108bfd66
rodolfosouzamoraes <rodolfomsouza@gmail.com>
2024-03-12 14:01:37 -0300

    Parallax

---
+++ Scripts/ParallaxBackground.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Godot;
+
+public partial class ParallaxBackground : Godot.ParallaxBackground
+{
+    double offsetloc = 0;
+    public override void _Ready()
+    {
+        SetProcess(true);
+    }
+
+    public override void _Process(double delta)
+    {
+        offsetloc += 150 * delta;
+        Vector2 motionMirroring = new Vector2(0, (float)offsetloc);
+        ScrollOffset = motionMirroring;
+    }
+}
